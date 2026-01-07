6d855605b821fa7c47696604ae437475eec01403
zallahve <91185216+zallahve@users.noreply.github.com>
2026-01-07 22:44:26 +0400

    Fix trailing space in Street fields for WHOIS CSV

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/^(Registrant|Admin|Tech)/ { if ($1 ~ /Street$/) print $1", "$2" "; else if ($1 ~ / Ext:$/ && NF==1) print $1","; else print $1", "$2 }' > $1.csv
+whois $1 | awk -F': ' '/^(Registrant|Admin|Tech)/ { if ($1 ~ /Street$/) printf "%s, %s \n", $1, $2; else if ($1 ~ / Ext:$/ && NF==1) print $1","; else print $1", "$2 }' > $1.csv
