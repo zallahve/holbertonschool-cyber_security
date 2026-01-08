d956304150cb9ea74b1882b0413d52a3c54d3e62
zallahve <91185216+zallahve@users.noreply.github.com>
2026-01-08 12:40:08 +0400

    Task 5: subfinder output to file

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -silent -d $1 | tee >(awk '{cmd="dig +short "$0" | head -n 1"; cmd | getline ip; close(cmd); if(ip!="") print $0","ip}' > $1.txt)
+subfinder -silent -d $1 -o $1.txt -nW -oI
