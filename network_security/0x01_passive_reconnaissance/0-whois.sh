fa81f85efb3da3f3d36a015a54ea349fd74ef7f0
zallahve <91185216+zallahve@users.noreply.github.com>
2026-01-09 16:39:03 +0400

    Fix 0-whois.sh to be 2 lines and match CSV format

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/^(Registrant|Admin|Tech)/{
-  gsub(/([a-z])([A-Z])/, "\\1 \\2", $1);
-  if ($1 ~ /Street$/)
-    printf "%s, %s \n", $1, $2;
-  else if ($1 ~ / Ext:$/ && NF == 1)
-    print $1",";
-  else
-    print $1", "$2
-}' > $1.csv
+whois $1 | awk -F': ' '/^(Registrant|Admin|Tech)/{gsub(/([a-z])([A-Z])/, "\\1 \\2", $1); if($1~/Street$/) printf "%s, %s \n",$1,$2; else if($1~/ Ext:$/ && NF==1) print $1","; else print $1", "$2}' > $1.csv
