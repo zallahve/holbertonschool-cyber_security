27cd21287021464eb52c990d2d7d168333f54271
zallahve <91185216+zallahve@users.noreply.github.com>
2026-01-12 22:24:48 +0400

    Add whois CSV extractor (task 0)

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' 'BEGIN{ORS="\n"} /^(Registrant|Admin|Tech)/{gsub(/([a-z])([A-Z])/, "\\1 \\2", $1); if($1~/Street$/) print $1", "$2" "; else if($1~/ Ext:$/ && NF==1) print $1","; else print $1", "$2}' > $1.csv
+whois "$1" | awk -F: '/^Registrant|^Admin|^Tech/ {if ($1 ~ /Ext/) $1=$1":"; if ($2=="") $2=" "; print $1 "," $2}' > "${1}.csv"
