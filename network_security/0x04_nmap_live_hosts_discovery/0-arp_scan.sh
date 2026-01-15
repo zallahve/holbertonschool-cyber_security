95d9f2f5e56f9a28b4153f05d5211c75f5a6ef17
zallahve <91185216+zallahve@users.noreply.github.com>
2026-01-15 20:58:47 +0400

    Fix 0-arp_scan.sh to meet line requirements

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,11 +1,2 @@
 #!/usr/bin/env bash
-# Discover live hosts on a subnet using ARP (no port scan)
-
-set -euo pipefail
-
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <subnet/CIDR>" >&2
-  exit 1
-fi
-
-nmap -sn -PR "$1"
+sudo nmap -sn -PR "$1"
