549e9a8b35463621fb18d87ecd71e0b95ab8c9ea
Ziya Allahverdiyev <YOUR_EMAIL_HERE>
2026-02-08 02:16:23 +0400

    Add john wordlist cracking script and output

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$(john --wordlist=<(gzip -dcf /usr/share/wordlists/rockyou.txt.gz /usr/share/wordlists/rockyou.txt 2>/dev/null) "$1" >/dev/null 2>&1)$(john --show "$1" 2>/dev/null | grep ':' | cut -d: -f2)" > 4-password.txt
+echo "$(john --wordlist=/usr/local/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1)$(john --show "$1" 2>/dev/null | grep ':' | cut -d: -f2)" > 4-password.txt
