6943581b4fb068fdadee6bbb0bfc2741d7dfa3bc
Ziya Allahverdiyev <YOUR_EMAIL_HERE>
2026-02-09 02:25:14 +0400

    Add hashcat straight cracking script

--- cybersecurity_basics/0x03_cryptography_basics/7-crack_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/7-crack_hashcat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$(hashcat -m 0 -a 0 "$1" "$(ls /usr/share/wordlists/rockyou.txt /usr/local/share/wordlists/rockyou.txt 2>/dev/null | head -n 1)" --quiet >/dev/null 2>&1)$(hashcat -m 0 --show "$1" --quiet 2>/dev/null | cut -d: -f2 | grep -v '^$' | head -n 1)" > 7-password.txt
+echo "$(hashcat -m 0 -a 0 <(tr -d '\r' < "$1") "$(ls /usr/share/wordlists/rockyou.txt /usr/local/share/wordlists/rockyou.txt 2>/dev/null | head -n 1)" --quiet >/dev/null 2>&1)$(hashcat -m 0 --show <(tr -d '\r' < "$1") --quiet 2>/dev/null | cut -d: -f2 | head -n 1)" > 7-password.txt
