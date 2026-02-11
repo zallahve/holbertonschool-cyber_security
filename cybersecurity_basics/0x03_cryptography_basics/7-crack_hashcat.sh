b10de8b4574964da0729345217b5b64d292bdd8c
Ziya Allahverdiyev <YOUR_EMAIL_HERE>
2026-02-11 16:51:15 +0400

    Add hashcat straight attack script and output

--- cybersecurity_basics/0x03_cryptography_basics/7-crack_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/7-crack_hashcat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$(hashcat -m 0 -a 0 <(tr -d '\r' < "$1") "$(ls /usr/share/wordlists/rockyou.txt /usr/local/share/wordlists/rockyou.txt 2>/dev/null | head -n 1)" --quiet >/dev/null 2>&1)$(hashcat -m 0 --show <(tr -d '\r' < "$1") --quiet 2>/dev/null | cut -d: -f2 | head -n 1)" > 7-password.txt
+echo "$(hashcat -m 0 -a 0 --potfile-path=./7.pot <(tr -d '\r' < "$1") "$(ls /usr/share/wordlists/rockyou.txt /usr/local/share/wordlists/rockyou.txt 2>/dev/null | head -n 1)" -O -w 1 --quiet >/dev/null 2>&1)$(hashcat -m 0 --show --potfile-path=./7.pot <(tr -d '\r' < "$1") --quiet 2>/dev/null | head -n 1 | cut -d: -f2)" > 7-password.txt
