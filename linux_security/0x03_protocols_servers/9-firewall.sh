3fa9853be70d67185d36fe23215fa91365ef33c6
Ziya Allahverdiyev <YOUR_EMAIL_HERE>
2026-03-30 03:04:56 +0400

    Add basic firewall rules script

---
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+iptables -P INPUT DROP
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT && iptables -A INPUT -i lo -j ACCEPT && iptables -A INPUT -p tcp --dport 22 -j ACCEPT
