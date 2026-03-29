8d8c46dd76e0e762b3c6f6848c6b99e3f406bcac
Ziya Allahverdiyev <YOUR_EMAIL_HERE>
2026-03-30 03:26:02 +0400

    Fix firewall script with explicit drop rule

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-iptables -P INPUT DROP
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT && iptables -A INPUT -i lo -j ACCEPT && iptables -A INPUT -p tcp --dport ssh -j ACCEPT
+iptables -A INPUT -p tcp --dport ssh -j ACCEPT
+iptables -A INPUT -j DROP
