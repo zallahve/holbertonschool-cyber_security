023d19629bed45483b08a4e832d3560a6245a7ed
Ziya Allahverdiyev <YOUR_EMAIL_HERE>
2026-03-30 02:32:06 +0400

    Fix hardening script to use xdev

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -type d -perm -0002 ! -perm -1000 -print -exec chmod o-w {} \; 2>/dev/null
+find / -xdev -type d -perm -0002 ! -perm -1000 -print -exec chmod o-w {} \; 2>/dev/null
