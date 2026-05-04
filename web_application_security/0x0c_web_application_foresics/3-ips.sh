bc2172e73f52192a61035a3a4f889cfc941fbcca
Ziya Allahverdiyev <YOUR_EMAIL_HERE>
2026-05-04 15:28:20 +0400

    Fix attacker IP count for compromised root account

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-grep -h "Accepted" * 2>/dev/null | awk '
-{
+awk '/Accepted/ && / for root / {
     for (i = 1; i <= NF; i++) {
         if ($i == "from") {
             print $(i + 1)
         }
     }
-}
-' | sort -u | wc -l | tr -d ' '
+}' auth.log 2>/dev/null | sort -u | wc -l | awk '{print $1}'
