f9bbf93de3214866b28af1b7a614cffed57fc8b6
Ziya Allahverdiyev <YOUR_EMAIL_HERE>
2026-05-04 15:33:30 +0400

    Count unique firewall rule additions

--- web_application_security/0x0c_web_application_foresics/4-firewall.sh
+++ web_application_security/0x0c_web_application_foresics/4-firewall.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-grep -Ei 'iptables.*[[:space:]]-A[[:space:]]|iptables.*--append|ufw.*(allow|deny|reject)|firewall-cmd.*--add' auth.log 2>/dev/null | wc -l | awk '{print $1}'
+awk '/COMMAND=/ && /iptables/ && / -A / {
+    sub(/^.*COMMAND=/, "")
+    print
+}' auth.log 2>/dev/null | sort -u | wc -l | awk '{print $1}'
