54a484a3dd734b8d6c2897a312699cfd26dc56f8
Ziya Allahverdiyev <YOUR_EMAIL_HERE>
2026-08-17 01:40:51 +0400

    Fix JSON merge output

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -7,4 +7,6 @@ def merge_json_files(file1_path, file2_path)
   merged_data = data2 + data1
 
   File.write(file2_path, JSON.pretty_generate(merged_data))
+
+  puts "Merged JSON written to #{file2_path}"
 end
