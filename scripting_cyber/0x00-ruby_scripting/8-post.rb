f7f137fa6c6ed2a4e47a72d03cd45d4e12bdfc3c
Ziya Allahverdiyev <YOUR_EMAIL_HERE>
2026-08-17 01:37:43 +0400

    Add HTTP POST request function

---
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
+def post_request(url, body_params = {})
+  uri = URI(url)
+
+  request = Net::HTTP::Post.new(uri)
+  request['Content-Type'] = 'application/json'
+  request.body = body_params.to_json
+
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+    http.request(request)
+  end
+
+  puts "Response status: #{response.code} #{response.message}"
+  puts "Response body:"
+  puts JSON.pretty_generate(JSON.parse(response.body))
+end
