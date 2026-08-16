4aff1ba4c90352ab8ec4ea9b7079661d88a2c544
Ziya Allahverdiyev <YOUR_EMAIL_HERE>
2026-08-17 01:44:34 +0400

    Fix arguments header output

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -2,6 +2,7 @@ def print_arguments
   if ARGV.empty?
     puts "No arguments provided."
   else
+    puts "Arguments:"
     ARGV.each_with_index do |arg, index|
       puts "#{index + 1}. #{arg}"
     end
