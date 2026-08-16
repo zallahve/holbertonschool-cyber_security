3712f1f618fd203fa0d50df27991414a312e488f
Ziya Allahverdiyev <YOUR_EMAIL_HERE>
2026-08-17 01:44:07 +0400

    Fix command-line arguments output

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -2,9 +2,8 @@ def print_arguments
   if ARGV.empty?
     puts "No arguments provided."
   else
-    puts "Arguments:"
-    ARGV.each do |arg|
-      puts arg
+    ARGV.each_with_index do |arg, index|
+      puts "#{index + 1}. #{arg}"
     end
   end
 end
